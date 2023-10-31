da9e82fd1a7bdd9c2affcbdd13152541c667096c
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-10-31 16:08:01 -0300

    Aula 01 - Booelan: Not

--- backend/aula1/tipos_booleanos.py
+++ backend/aula1/tipos_booleanos.py
@@ -1,8 +1,17 @@
 # e , ou
 
+# And
 print(True and True)
 print(True and False)
+print(False and False)
 
+# OR
 print(True or False)
 print(False or False)
+print(True or True)
+
+# NOT
+print(not True)
+print(not False)
+
 
